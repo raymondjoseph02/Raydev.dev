868223073cf80f4a2129e083f0752368bff2b763
raymondjoseph02 <amraymondjoseph@gmail.com>
2025-09-14 15:54:58 +0100

    n

--- src/components/portfolio/Hero.tsx
+++ src/components/portfolio/Hero.tsx
@@ -6,10 +6,10 @@ function Hero({ activeTab, setActiveTab, tabs }: PortfolioHeroProps) {
   return (
     <section className="pt-20 sm:pt-28 pb-18 sm:pb-40.5">
       <div className="flex items-center justify-center flex-col container-wrapper gap-11">
-        <h1 className="text-4xl sm:text-8xl font-normal text-white">
+        <h1 className="text-4xl sm:text-8xl font-normal text-white text-center">
           Some highlights of my work
         </h1>
-        <div className="flex gap-2">
+        <div className="flex gap-2 px-5 sm:px-0">
           {tabs?.map((tab, index) => {
             const active = activeTab.toLowerCase() === tab.toLowerCase();
             return (

d7f52f5d8bd3765020d704f2267b19093230b184
raymondjoseph02 <amraymondjoseph@gmail.com>
2025-09-14 15:57:28 +0100

    n

--- src/components/portfolio/Hero.tsx
+++ src/components/portfolio/Hero.tsx
@@ -4,9 +4,9 @@ import { PortfolioHeroProps } from "@/types/types";
 
 function Hero({ activeTab, setActiveTab, tabs }: PortfolioHeroProps) {
   return (
-    <section className="pt-20 sm:pt-28 pb-18 sm:pb-40.5">
-      <div className="flex items-center justify-center flex-col container-wrapper gap-11">
-        <h1 className="text-4xl sm:text-8xl font-normal text-white text-center">
+    <section className="pt-24 sm:pt-28 pb-18 sm:pb-40.5">
+      <div className="flex items-center justify-center flex-col container-wrapper gap-8 sm:gap-11">
+        <h1 className="text-4xl sm:text-8xl font-medium sm:font-normal text-white text-center">
           Some highlights of my work
         </h1>
         <div className="flex gap-2 px-5 sm:px-0">

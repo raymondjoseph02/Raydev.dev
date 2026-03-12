d7469142ec5183ac50a917d98a2060566d11f7f5
raymondjoseph02 <amraymondjoseph@gmail.com>
2026-03-12 10:38:54 +0100

    fix social media link

--- src/components/ui/Socials.tsx
+++ src/components/ui/Socials.tsx
@@ -2,17 +2,17 @@ import Link from "next/link";
 import { FaGithub, FaLinkedin, FaXTwitter } from "react-icons/fa6";
 export const socials = [
   {
-    link: "/",
+    link: "https://x.com/RayDev02",
     icon: <FaXTwitter />,
     label: "Twitter",
   },
   {
-    link: "/",
+    link: "https://www.linkedin.com/in/raydev02/",
     icon: <FaLinkedin />,
     label: "Linkedin",
   },
   {
-    link: "/",
+    link: "https://github.com/raymondjoseph02",
     icon: <FaGithub />,
     label: "Github",
   },
@@ -26,6 +26,7 @@ function Socials() {
             aria-label={social.label}
             className="size-10 rounded-full justify-center hover:bg-[#242424] text-white flex items-center transition ease-in-out duration-300 will-change-auto text-2xl"
             href={social.link}
+            target="_blank"
           >
             {social.icon}
           </Link>
